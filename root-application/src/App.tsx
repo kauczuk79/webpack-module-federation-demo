91bfbf3e27ab98336023087cf0a8c4d8a7534837
Robert Wesołowski <kauczuk79@gmail.com>
2025-02-25 21:29:31 +0100

    React module sync load

--- root-application/src/App.tsx
+++ root-application/src/App.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+import ReactChild from "ReactChild/App";
+
+const ReactChildAppComponent = require("ReactChild/App")
+  .default as typeof ReactChild;
+
 export default function App() {
-  return <div>Root application [React 19]</div>;
+  return (
+    <div>
+      Root application [React 19]
+      <ReactChildAppComponent></ReactChildAppComponent>
+    </div>
+  );
 }
