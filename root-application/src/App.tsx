c6a04163ac9d25ff7244238ff20e539ba299c90a
Robert Wesołowski <kauczuk79@gmail.com>
2025-02-25 21:02:29 +0100

    Empty root application

---
+++ root-application/src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div>Root application [React 19]</div>;
+}
---
+++ root-application/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    historyApiFallback: {
+      index: "/public/index.html",
+    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
