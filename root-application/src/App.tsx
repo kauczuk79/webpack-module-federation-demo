be978316900c512a94c121647a7d8940991147aa
Robert Wesołowski <kauczuk79@gmail.com>
2025-02-25 21:51:39 +0100

    Lazy loading of the module

--- root-application/src/App.tsx
+++ root-application/src/App.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 
 import ReactChild from "ReactChild/App";
 
-const ReactChildAppComponent = require("ReactChild/App")
-  .default as typeof ReactChild;
+const ReactChildAppComponent = lazy<typeof ReactChild>(async () => {
+  try {
+    return await import("ReactChild/App");
+  } catch {
+    return { default: (): React.JSX.Element => <div>Error</div> };
+  }
+});
 
 export default function App() {
   return (
     <div>
       Root application [React 19]
-      <ReactChildAppComponent></ReactChildAppComponent>
+      <Suspense fallback={<div>Loading module...</div>}>
+        <ReactChildAppComponent></ReactChildAppComponent>
+      </Suspense>
     </div>
   );
 }
